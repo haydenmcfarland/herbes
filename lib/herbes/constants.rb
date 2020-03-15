d16c9600687d9ed685dd3e361b016079328d20cf
Hayden McFarland <mcfarlandsms@gmail.com>
2020-03-15 12:01:12 -0700

    remove old build constants

--- lib/herbes/constants.rb
+++ lib/herbes/constants.rb
@@ -2,9 +2,6 @@
 
 module Herbes
   module Constants
-    BUILD_NL = "\n"
-    BUILD_SPACING = 2
-    BUILD_PADD = BUILD_NL + ' ' * BUILD_SPACING
     DEFAULT_EMAIL_TEMPLATE_PATH = 'lib/herbes/assets/templates/herbes.html.erb'
     DEFAULT_EMAIL_STYLE_PATH = 'lib/herbes/assets/styles/herbes.css'
   end
